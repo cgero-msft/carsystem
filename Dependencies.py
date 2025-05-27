9cf9da2cdcbc215a7f2d8c7522f3da43f0753eb9
Christian Gero <christian.gero@microsoft.com>
2025-05-27 14:27:47 -0500

    Create Dependencies.py

---
+++ Dependencies.py
@@ -0,0 +1,14 @@
+#PACKAGES REQUIRED
+
+sudo apt install python3-pip
+pip3 install opencv-python
+pip3 install tkinter #this one throws an error for some reason: ERROR: Could not find a version that satisfies the requirement tkinter
+, ERROR: No matching distribution found for tkinter
+pip3 install adafruit-circuitpython-pca9685
+pip3 install pynput
+
+
+
+sudo apt-get install libcblas-dev #this makes it work
+
+
